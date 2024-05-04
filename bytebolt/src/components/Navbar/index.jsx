c438215ca615d3d69ea728b0320c7ddcc2801294
ByteBolt21 <bytebolt.info@gmail.com>
2024-05-04 20:47:53 +0500

    Component and pages

---
+++ bytebolt/src/components/Navbar/index.jsx
@@ -0,0 +1,18 @@
+// Navigation.js
+
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#services">Services</a></li>
+                <li><a href="#about">About Us</a></li>
+                <li><a href="#contact">Contact Us</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
---
+++ bytebolt/src/pages/Home/index.jsx
@@ -0,0 +1,19 @@
+
+
+
+import React from 'react';
+import Navbar from './components/Navbar/index.jsx';
+
+function Home() {
+    return (
+        <div>
+            <header>
+                <Navbar />
+            </header>
+        </div>
+
+    );
+}
+
+export default Home;
+
